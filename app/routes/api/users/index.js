dd6dd37d74776955b7ade2a6462c7892867b096e
Kristina Lyalina <klyalina@imiit.ru>
2019-01-19 16:51:01 +0300

    basic users get/add/delete

--- app/routes/api/users/index.js
+++ app/routes/api/users/index.js
@@ -3,18 +3,56 @@ import models from '../.././../db/models';
 
 const router = express.Router();
 
-router.get('/', (req, res) => {
+router.get('/getAll', (req, res) => {
     models.User.findAll().then((users) => {
         res.send(users);
     });
 });
 
-router.get('/add', (req, res) => {
-    models.User.create({
-        firstName: 'Vasiliy',
-        lastName: 'Velikolepniy',
-        phone: '8-800-555-35-35'
-    });
-})
+router.post('/add', (req, res) => {
+    const { firstName, lastName, phone } = req.query;
+
+    if (!firstName
+        || !lastName) {
+        res.send({ error: "firstName and lastName is required" })
+    } else {
+        models.User.create({ firstName, lastName, phone }).then(user => {
+            res.send({
+                id: user.id
+            });
+        });
+    }
+});
+
+router.post('/delete', (req, res) => {
+    const { id } = req.query;
+
+    if (id) {
+        models.User.findByPk(id).then(user => {
+            if (user) {
+                user.destroy().then((result) => {
+                    res.send({ message: `user id=${id} deleted` });
+                });
+            } else {
+                res.send({ error: "user not found" });
+            }
+        });
+    } else {
+        res.send({ error: 'id is not specified' });
+    }
+
+});
+
+router.get("/get", (req, res) => {
+    const id = req.query.id;
+
+    if (id) {
+        models.User.findByPk(id).then((user) => {
+            user? res.send(user) : res.send({ error: `user with id=${id} is not found` });
+        });
+    } else {
+        res.send({ error: 'id is not set' });
+    }
+});
 
 export default router;
